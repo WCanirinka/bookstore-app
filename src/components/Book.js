491164cbbac9c7798c45bcfcad481d9c3ab9516c
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-04-30 18:38:22 +0200

    Add Book and BookList Components to Bookstore

---
+++ src/components/Book.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Book({ book }) {
+  return (
+    <tr>
+      <td>{ book.id }</td>
+      <td>{ book.title}</td>
+      <td>{ book.category }</td>
+    </tr>
+  );
+}
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,16 +1,40 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import Book from '../components/Book';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Book from '../components/Book';
 
-export default function BooksList() {
+
+export default function BooksList({ books }) {
+  const displayBooks = books.length > 0 ? (books.map(book => (
+    <Book book={book} key={book.id} />
+  ))) : null;
+
   return (
     <table>
       <thead>
-        <th>Book ID</th>
-        <th>Title</th>
-        <th>Category</th>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
       </thead>
-      {  }
+      <tbody>
+        { displayBooks }
+      </tbody>
     </table>
   );
 }
+
+BooksList.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const mapStateToProps = ({ booksReducer: { books } }) => ({
+  books,
+});
+connect(mapStateToProps)(BooksList);
